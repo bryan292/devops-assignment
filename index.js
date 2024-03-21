4f32c46b10c45df8fb4ce533cdebaa29c4f257be
RichTechOps <RichTechOps@users.noreply.github.com>
2024-03-21 14:32:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require(‘http’);
 const hostname = ‘localhost’;
 const port = 3000;
-const appName = process.env.APP_NAME || 'Default name';
+const appName = process.env.APP_NAME || '';
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
