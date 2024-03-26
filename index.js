e1763663f8de07a4c3c05e5d62b0b5417714cd2c
Rich Cusimano <rcusimano@ingenio.com>
2024-03-26 14:24:03 -0700

    fixed formatting of var

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const http = require(‘http’);
-const hostname = ‘localhost’;
+const http = require('http');
+const hostname = 'localhost';
 const port = 3000;
-const appName = process.env.APP_NAME || '';
+const appEnv = process.env.APP_ENV || 'No env specified';
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
- res.end(‘<html><body><h1>Hello, World ${appName}</h1></body></html>’);
+ res.end("<html><body><h1>Hello, World ${appEnv}</h1></body></html>");
 })
 server.listen(port, hostname);
-
